5013669cf9c17121398f31d512c52b116100d24d
Earthbert <daraban_timotei@tutanota.com>
2023-04-22 12:28:36 +0300

    Add receiving udp packet

--- src/include/common.h
+++ src/include/common.h
@@ -7,12 +7,17 @@
 int send_all(int sockfd, void *buff, size_t len);
 int recv_all(int sockfd, void *buff, size_t len);
 
-/* Dimensiunea maxima a mesajului */
-#define MSG_MAXSIZE 1024
+#define PAYLOAD_MAXSIZE 1500
 
-struct chat_packet {
-  uint16_t len;
-  char message[MSG_MAXSIZE + 1];
+struct __attribute__((__packed__)) udp_client_packet {
+  char topic[50];
+  u_int8_t data_type;
+  char payload[PAYLOAD_MAXSIZE];
+};
+
+struct udp_client_info {
+  struct udp_client_packet packet;
+  sockaddr_in addr;
 };
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -8,9 +8,24 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <netinet/tcp.h>
 
 #include "die.h"
 #include "common.h"
+
+#define MAX_CONNECTIONS 100
+
+int receive_topic(int fd, struct udp_client_info *udp_info) {
+	socklen_t udp_client_addr_len = sizeof(udp_info->addr);
+	int rc = recvfrom(fd, &(udp_info->packet), sizeof(udp_info->packet), 0,
+		(struct sockaddr *)&(udp_info->addr), &udp_client_addr_len);
+
+	return rc;
+}
+
+void handle_topic(udp_client_info *udp_info) {
+	printf("%s %hhd %s\n", udp_info->packet.topic, udp_info->packet.data_type, udp_info->packet.payload);
+}
 
 int main(int argc, char const *argv[]) {
 	if (argc != 2) {
@@ -18,8 +33,13 @@ int main(int argc, char const *argv[]) {
 		return 1;
 	}
 
+	// Disable stdout buffering
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
 	struct sockaddr_in servaddr;
 	int tcp_fd, udp_fd, rc;
+
+	struct udp_client_info udp_p;
 
 	// Save server port number as number
 	uint16_t port;
@@ -37,7 +57,7 @@ int main(int argc, char const *argv[]) {
 	// Make port reusable, in case we run this really fast two times in a row
 	{
 		int enable = 1;
-		setsockopt(tcp_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+		setsockopt(tcp_fd, SOL_SOCKET, SO_REUSEADDR | TCP_NODELAY, &enable, sizeof(int));
 		DIE(tcp_fd < 0, "setsockopt(SO_REUSEADDR) failed");
 
 		setsockopt(udp_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
@@ -56,7 +76,31 @@ int main(int argc, char const *argv[]) {
 	rc = bind(udp_fd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 	DIE(rc < 0, "udp failed");
 
-	
+	// Set up poll
+	struct pollfd poll_fds[MAX_CONNECTIONS];
+	int nr_conn = 2;
+	// Add both server sockets to poll
+	poll_fds[0].fd = tcp_fd;
+	poll_fds[0].events = POLLIN;
+	poll_fds[1].fd = udp_fd;
+	poll_fds[1].events = POLLIN;
+
+	while (1) {
+		poll(poll_fds, nr_conn, -1);
+
+		for (int i = 0; i < nr_conn; i++) {
+			if (poll_fds[i].revents & POLLIN) {
+				if (poll_fds[i].fd == udp_fd) {
+					rc = receive_topic(poll_fds[i].fd, &udp_p);
+					if (rc > 0)
+						handle_topic(&udp_p);
+					else {
+						fprintf(stderr, "udp client topic submission error\n");
+					}
+				}
+			}
+		}
+	}
 
 	return 0;
 }
